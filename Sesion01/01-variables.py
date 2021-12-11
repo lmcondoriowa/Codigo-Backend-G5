6b8166be5d10a19fad12a638867e72a9842fb53b
Luis Condori Dell 5593 <lmcondori@outlook.com>
2021-12-10 19:56:42 -0500

    Variables

---
+++ Sesion01/01-variables.py
@@ -0,0 +1,43 @@
+# variable numerica
+numero = 10
+numero2 = 10.5
+
+numero = "Febrero"
+
+# variables de texto  o string
+nombre = "Luis"
+apellido = "Condori"
+html = '''
+'''
+
+html = """
+"""
+
+fecha = 0
+
+print('hola :)')
+
+print(type(nombre))
+#str = string
+# int = integer
+#float = float
+print(type(numero))
+print(type(numero2))
+#bool = boolean
+
+soltero = True
+calor=False
+print(type(soltero))
+
+# VARIABLES que tienen varios VALORES
+# ARREGLOS > LISTAS LIST
+edades = [ 10, 12 ,40 , 'Luis', 15.1, False]
+
+# JSON (JavaScript Object Notation) | Diccionario
+curso = {
+    'nombre': 'Backend',
+    'nombre': 'Frontend',
+    'dificultad': 'Dificil',
+}
+
+print(curso['dificultad'])
