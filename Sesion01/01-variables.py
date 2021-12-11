dc7ec97510f310f7205b88594240dad3123131cd
Luis Condori Dell 5593 <lmcondori@outlook.com>
2021-12-10 21:48:42 -0500

    chore: inicio de operadores

--- Sesion01/01-variables.py
+++ Sesion01/01-variables.py
@@ -32,12 +32,61 @@ print(type(soltero))
 # VARIABLES que tienen varios VALORES
 # ARREGLOS > LISTAS LIST
 edades = [ 10, 12 ,40 , 'Luis', 15.1, False]
+print(edades[1:20])
 
 # JSON (JavaScript Object Notation) | Diccionario
 curso = {
     'nombre': 'Backend',
     'nombre': 'Frontend',
     'dificultad': 'Dificil',
+    'skills': [
+        {
+            'nombre':'Base de Datos',
+            'nivel':'Intermedio'
+        },
+        {
+            'nombre':'ORM',
+            'nivel':'Avanzado'
+        }
+    ]
 }
 
 print(curso['dificultad'])
+
+# quiero el nivel del skill ORM
+print(curso['skills'][1]['nivel'])
+
+personas = [{
+    'nombre': 'Eduardo',
+    'nacionalidad': 'peruano',
+    'hobbies':[
+        {
+            'nombre': 'Volar drones',
+            'experiencia': '2 años'
+        },{
+            'nombre':'Programar',
+            'experiencia': '1 mes'
+        }
+    ]
+},{
+    'nombre': 'Juliana',
+    'nacionalidad': 'colombiana',
+    'hobbies':[
+        {
+            'nombre': 'Montar bici',
+            'experiencia': '4 años'
+        },{
+            'nombre':'Bases de datos',
+            'experiencia': '8 mes'
+        }
+    ]
+}]
+
+# nacionalidad de la primera persona
+print(personas[0]['nacionalidad'])
+
+# los hobbies de la segunda persona
+print(personas[1]['hobbies'])
+
+# la experiencia del segundo hobbie de la primera persona
+print(personas[0]['hobbies'][1]['experiencia'])
